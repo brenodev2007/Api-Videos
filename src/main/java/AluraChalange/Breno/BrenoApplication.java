0c48daffe9f0bfcf462e525072f166d2ad9e1eb1
Breno <breno.soriani@gmail.com>
2025-02-24 16:11:17 -0300

    first commit

---
+++ src/main/java/AluraChalange/Breno/BrenoApplication.java
@@ -0,0 +1,13 @@
+package AluraChalange.Breno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrenoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrenoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AluraChalange/Breno/BrenoApplicationTests.java
@@ -0,0 +1,13 @@
+package AluraChalange.Breno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrenoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
