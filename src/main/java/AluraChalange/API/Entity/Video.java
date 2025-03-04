ef9f6a6d00c682296141acff6800101955a9385a
Breno <breno.soriani@gmail.com>
2025-03-04 11:13:05 -0300

    Categoria Entity implementacion

--- src/main/java/AluraChalange/API/Entity/Video.java
+++ src/main/java/AluraChalange/API/Entity/Video.java
@@ -1,11 +1,6 @@
 package AluraChalange.API.Entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
@@ -24,7 +19,10 @@ public class Video {
     public String descricao;
     @Column(name = "url", nullable = false)
     private String url;
-    
+
+    @JoinColumn
+    @ManyToOne
+    private Categoria categoria;
 
 
 
